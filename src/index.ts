91cbd16f23f6fa688c8cdd4be9f53553d81f2ef5
xinan.wang <xinan.wang@chiefclouds.com>
2019-11-28 10:27:50 +0800

    add commander colors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 // import * as commander from 'commander';
 // const program = require('commander');
-import { Command } from 'commander';
-const command = new Command();
-command
+var colors = require('colors');
+// import colors from 'colors';
+import commander from 'commander';
+// const command = new Command();
+commander
     .version('0.1.0')
     .option('-p, --peppers [name]', 'Add peppers')
     .option('-P, --pineapple', 'Add pineapple')
     .option('-b, --bbp-sauce', 'Add bbq sauce')
-    // .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-    .option('-c, --city [name]', 'Add city name');
-command.parse(process.argv);
-
-// if (!command.city) {
-//     command.outputHelp();
+    .option('-c, --city [name]', 'Add city name')
+    .parse(process.argv);
+// if (!commander.city) {
+//     commander.outputHelp();
+// }
+// if (commander.city) {
+//     console.log(commander.city.green)
 // }
 if (process.argv.slice(2).length === 0) {
-    command.outputHelp()
-    process.exit()
+    commander.outputHelp(colors.red)
+    process.exit();
 }
